cf5174605a4eabc9ffa7c15ec2c4aaf5a1ef5897
Jhon-crypt <johnoladele690@gmail.com>
2024-01-31 00:56:52 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/header/header'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+
+    return (
+
+        <>
+
+            <nav className="navbar">
+                <div className="container">
+                    <a className="navbar-brand" href="#">
+                        EasyBranding
+                    </a>
+                </div>
+            </nav>
+
+        </>
+
+    )
+
+}
